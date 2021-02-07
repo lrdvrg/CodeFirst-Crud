1b616599cad1ffc38c6acde4a0a395a285d79a95
Isai Vargas <isaidvargas@hotmail.com>
2021-02-06 23:58:13 -0400

    subir tarea de codefirst

---
+++ MVCCrud/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCCrud.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class  CompanyDBContext: DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
